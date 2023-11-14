ac78b1a6a4854d5e8ce712557d3778ba8926448a
JosueKH <>
2023-11-14 15:53:15 -0300

    Adicionado Página Detalhes

---
+++ src/main/java/com/josuehoffmann/projetointegrador/controller/LivroController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.josuehoffmann.projetointegrador.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Koffmann
+ */
+@Controller
+public class LivroController {
+    @GetMapping("/detalhes")
+    public String exibirTest(Model model) {
+        return "detalhes";
+    }
+}
